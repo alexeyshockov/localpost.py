222617dfcfe6c483206196b2fddd631e366620c6
Alexey Shokov <alexey@shokov.dev>
2026-03-05 17:16:37 +0000

    WIP on examples

--- examples/http/openapi_app_server.py
+++ examples/http/openapi_app_server.py
@@ -1,18 +1,30 @@
+from dataclasses import dataclass
 from wsgiref.simple_server import make_server
 
-from localpost.http.openapi.app import HttpApp
+from localpost.http.openapi.app import BadRequest, HttpApp
+
+
+@dataclass()
+class Book:
+    id: str
+    title: str
+    author: str
+    page: int
 
 
 def main():
     app = HttpApp()
 
     @app.get("/hello/{name}")
-    def hello(name: str) -> str:
+    def hello(name: str) -> str | BadRequest[str]:
+        if name == "Donald":
+            return BadRequest("Sorry, you are not welcome here")
+
         return f"Hello, {name}!"
 
     @app.get("/books/{book_id}")
-    def get_book(book_id: str, page_number: int = 1) -> dict:
-        return {"id": book_id, "title": "The Lord of the Rings", "author": "J.R.R. Tolkien", "page": page_number}
+    def get_book(book_id: str, page_number: int = 1) -> Book:
+        return Book(id=book_id, title="The Lord of the Rings", author="J.R.R. Tolkien", page=page_number)
 
     print("Starting server on http://localhost:8000")
     print("Try: curl http://localhost:8000/hello/world")
