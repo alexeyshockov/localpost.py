65a6cfa38ad7952179f0a592927b5a08c26eee0c
Alexey Shokov <alexey@shokov.dev>
2026-03-22 09:54:45 +0000

    cleanup

--- tests/consumers/conftest.py
+++
@@ -1,6 +0,0 @@
-import logging
-
-from testcontainers.core import utils
-
-# See https://github.com/testcontainers/testcontainers-python/pull/758
-utils.setup_logger = logging.getLogger
--- tests/consumers/stream.py
+++ tests/consumers/stream.py
@@ -1,58 +1,38 @@
-import anyio
 import pytest
-from anyio import WouldBlock, create_memory_object_stream, create_task_group, sleep
+from anyio import create_memory_object_stream, sleep
 
-from localpost import flow
 from localpost.consumers.stream import stream_consumer
-from localpost.flow import batch
-from localpost.hosting import Host
+from localpost.hosting import serve
 
-pytestmark = [pytest.mark.anyio, pytest.mark.integration]
+pytestmark = [pytest.mark.anyio]
 
 
 @pytest.fixture()
-async def local_queue():
+def local_queue():
     writer, reader = create_memory_object_stream(10)
-    yield writer, reader
+    return writer, reader
 
 
 async def test_normal_case(local_queue):
     writer, reader = local_queue
 
     # Arrange
-
     sent = ["hello", "world", "!"]
     for message in sent:
         writer.send_nowait(message)
 
     # Act
-
     received = []
 
-    @stream_consumer(reader)
-    @flow.handler
     async def handle(m: str):
-        nonlocal received
-        received += [m]
+        received.append(m)
 
-    host = Host(handle)
-    async with host.aserve():
-        await anyio.sleep(0.5)  # "App is working"
+    consumer = stream_consumer(reader, handle)
+
+    async with serve(consumer) as lt:
+        await sleep(0.5)
         writer.close()
+        await lt.stopped
 
     # Assert
-
-    assert host.status["exception"] is None
     assert received == sent
-
-
-async def test_batching(local_queue):
-    writer, reader = local_queue
-
-    # TODO Implement
-
-
-async def test_buffering(local_queue):
-    writer, reader = local_queue
-
-    # TODO Implement
